5b1fa69ceab5a9e11d1d1d7b0125ef2a5f4b7d1c
Alamagan <ali_shumdar@hotmail.com>
2024-03-05 03:12:43 +0000

    first commit

---
+++ variables_datatypes.js
@@ -0,0 +1,63 @@
+// var firstName = "Alamagan"
+// console.log(firstName)
+
+
+// var lastName ="Khalif"
+// console.log(lastName)
+
+
+// var age = 8
+// console.log(age)
+
+
+// var isStudent = true 
+// console.log(isStudent)
+
+// var isStudent = false 
+// console.log(isStudent)
+
+
+
+// console.log(typeof(isStudent))
+// console.log(typeof(age))
+
+
+
+// var boss = 300;
+// var ayanle = 30;
+// var gunner = boss - ayanle;
+// console.log(gunner)
+
+
+// var firstName = "alamaagn"
+// console.log(firstName)
+
+
+
+// // var firstName = "alamagan"
+// // console.log(firstName)
+
+// // var lastName = "khalif"
+// // console.log(lastName)
+
+// // var age = 20
+// // console.log(age)
+
+// // var isLightOn = false
+// // console.log(isLightOn)
+
+// // var isLightOn = true 
+// // console.log(isLightOn)
+
+
+// var wage = 5367;
+// var tax = 2134;
+// var takeHome = wage - tax;
+// console.log(takeHome)
+
+
+// var list = {
+//     name : "alamagan",
+//     birth: "england",
+//     smoke: false
+// }
